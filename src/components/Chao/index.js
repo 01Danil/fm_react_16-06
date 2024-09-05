868660d0c0425d2bac8365dfd6f1f62119019be5
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2024-09-05 20:24:27 +0100

    Refactor App to class

--- src/components/Chao/index.js
+++ src/components/Chao/index.js
@@ -1,24 +1,24 @@
 import { Component } from "react";
 
 class Chao extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
-    this.state = { isHi: true };
+    this.state = {isHi:true};
   }
-	switchState = () => {
-		const { isHi } = this.state;
-		this.setState({isHi:!isHi})
-	};
+  switchState = ()=>{ 
+    const {isHi} = this.state;   
+    this.setState({isHi:!isHi}); 
+  }
   render() {
-    const { name } = this.props;
-    const { isHi } = this.state;
-    // const wordHi = isHi ? "Hi" : "Bye";
-    return (
-      <>
-        <h2>{isHi ? "Hi" : "Bye"}! {name}!</h2>
+    const {name} = this.props;
+    const {isHi} = this.state;
+    if(isHi){
+      return <>
+        <h2>Hi! {name}!</h2>
         <button onClick={this.switchState}>Switch</button>
       </>
-    );
+    }
+    return <p>Bye, {name}!</p>;
   }
 }
 
